37cb87f69e31e987ea5f86812ad08f382bc22e50
Josh Elias <josh@daemontech.tools>
2023-11-20 15:44:51 -0500

    add first Component and Systems

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("I'm a Rust");
-    println!("I'm a big boy....NOW....again...more!!!!");
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn hello_world() {
+    println!("hello world");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Leeroy Jenkins".to_string())));
+    commands.spawn((Person, Name("Stan Lee".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
 }
